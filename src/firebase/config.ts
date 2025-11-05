09e8bfc42efe85a5fa4289e498808e0a22086090
Benji Muziq <benjimuziqsings@gmail.com>
2025-11-05 20:46:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "the-bailey-collective",
+  "appId": "1:433553504735:web:6e705b2c88c1f27b73ac6a",
+  "apiKey": "AIzaSyDrhXLHEPglKYIV5S4uWybNoKO8rFU7uL8",
+  "authDomain": "the-bailey-collective.firebaseapp.com",
+  "measurementId": "G-GG72JKX1WR",
+  "messagingSenderId": "433553504735"
+};
