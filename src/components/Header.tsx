5b5d55506eb6a51d99c08ecf5db134fb38c2e94b
Benji Muziq <benjimuziqsings@gmail.com>
2025-11-08 16:33:09 +0000

    on user profile do not show admin button or kj view... only show kj view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,10 @@ import { useUser } from '@/firebase';
 
 export function Header({ isAdmin = false }: { isAdmin?: boolean }) {
   const { user } = useUser();
+  
+  const adminEmails = ['benjaminbailey98@gmail.com', 'benjimuziqsings@gmail.com'];
+  const isAuthorizedKJ = user && user.email && adminEmails.includes(user.email);
+
   return (
     <header className="py-3 px-4 md:px-6 border-b border-border/50 sticky top-0 bg-background/80 backdrop-blur-sm z-20">
       <div className="container mx-auto flex justify-between items-center">
@@ -34,19 +38,23 @@ export function Header({ isAdmin = false }: { isAdmin?: boolean }) {
               </Button>
             )}
             {isAdmin && <QRCodeDialog />}
-          <Button asChild variant="ghost" size="sm">
-            {isAdmin ? (
-              <Link href="/home">
-                <Users className="mr-2 h-4 w-4" />
-                Patron View
-              </Link>
-            ) : (
-              <Link href="/admin">
-                <UserCog className="mr-2 h-4 w-4" />
-                KJ View
-              </Link>
-            )}
-          </Button>
+          
+          {isAuthorizedKJ && (
+             <Button asChild variant="ghost" size="sm">
+              {isAdmin ? (
+                <Link href="/home">
+                  <Users className="mr-2 h-4 w-4" />
+                  Patron View
+                </Link>
+              ) : (
+                <Link href="/admin">
+                  <UserCog className="mr-2 h-4 w-4" />
+                  KJ View
+                </Link>
+              )}
+            </Button>
+          )}
+
         </nav>
       </div>
     </header>
