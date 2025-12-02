9d7194d9e002a6412e2353cd2417d2b39a119971
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-02 16:51:24 +0000

    error: flake 'git+file:///home/user/studio' does not provide attribute '

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
-
 {
-  description = "A deployment flake for a Next.js application.";
+  description = "Singalong Karaoke Deployment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     deploy-rs.url = "github:serokell/deploy-rs";
   };
@@ -11,53 +10,45 @@
   outputs = { self, nixpkgs, flake-utils, deploy-rs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs_20 # Provides node, npm, etc.
-            pkgs.deploy-rs
-          ];
-        };
-
         packages.deploy = deploy-rs.lib.${system}.mkDeployment {
           inherit pkgs;
-
           deployment.name = "singalong-karaoke-deployment";
 
-          deployment.steps = [
-            (pkgs.runCommand "yarn-deps" { } ''
-              mkdir -p $out
-              cp package.json yarn.lock $out/ || true
-              cp package.json $out/ || true
-            '')
-            (pkgs.runCommand "build-app" { } ''
-              # Copy source files
-              cp -r ${./.}/* .
-
-              # Install dependencies and build
-              npm install
-              npm run build
-
-              # Copy build artifacts to output
-              mkdir -p $out
-              cp -r ./.next $out/
-              cp -r ./node_modules $out/
-              cp ./package.json $out/
-              cp ./next.config.js $out/
-              cp -r ./public $out/ || true
-            '')
-          ];
-
-          service = {
-            name = "singalong-karaoke";
-            exec = "npm run start";
+          deployment.build = {
+            copy = [
+              ./apphosting.yaml
+              ./package.json
+              ./package-lock.json
+              ./next.config.js
+              ./tsconfig.json
+              ./public
+              ./src
+            ];
+            buildCommands = [
+              ''
+                # Remove node_modules if it exists to ensure a clean install
+                rm -rf node_modules
+                # Set a specific path for the npm cache
+                export NPM_CONFIG_CACHE=$(mktemp -d)
+                npm install --legacy-peer-deps
+                npm run build
+              ''
+            ];
           };
 
-          rollback.magic = true;
+          deployment.start = {
+            exec = "npm run start";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_20
+            pkgs.deploy-rs
+          ];
         };
       });
 }
