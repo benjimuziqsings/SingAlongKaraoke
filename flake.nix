2070e6d1ae5892544e027f09caab1a30becc5170
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-02 14:22:10 +0000

    error: path '/home/user/studio' is not part of a flake (neither it nor i

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  "description": "A Next.js application for karaoke song requests.",
+  "inputs": {
+    "nixpkgs": {
+      "url": "github:NixOS/nixpkgs/nixos-unstable"
+    },
+    "flake-utils": {
+      "url": "github:numtide/flake-utils"
+    }
+  },
+  "outputs": { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        "devShell": pkgs.mkShell {
+          "buildInputs": [
+            pkgs.nodejs_20
+            pkgs.deploy-rs
+          ];
+        };
+      }
+    )
+}
