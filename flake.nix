980141deacb0305df8ec69f42cc8337b185f1821
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-02 15:07:41 +0000

    ERROR ERROR: build step 1 "us-central1-docker.pkg.dev/serverless-runtime

--- flake.nix
+++ flake.nix
@@ -3,51 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, deploy-rs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.nodejs_20
-            deploy-rs.packages.${system}.deploy-rs
-          ];
+  outputs = { self, nixpkgs, deploy-rs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_20
+          deploy-rs.packages.${system}.deploy-rs
+        ];
+      };
+
+      packages.${system}.deploy = deploy-rs.lib.${system}.mkDeployment {
+        inherit pkgs;
+        deployment.name = "singalong-karaoke-deployment";
+
+        steps = [
+          {
+            name = "Install Dependencies";
+            run = "npm install";
+          }
+          {
+            name = "Build Next.js App";
+            run = "npm run build";
+          }
+        ];
+
+        service = {
+          exec = "npm run start";
+          user = "root";
         };
 
-        packages.${system}.deploy = deploy-rs.lib.${system}.mkDeployment {
-          inherit pkgs;
-          deployment = {
-            name = "singalong-karaoke";
-            user = "root";
-            userHome = "/root";
-            
-            steps = [
-              {
-                name = "Install dependencies";
-                run = "npm install";
-              }
-              {
-                name = "Build Next.js app";
-                run = "npm run build";
-              }
-            ];
-            
-            services.app = {
-              type = "simple";
-              exec = "npm run start";
-            };
-
-            rollback.magic = true;
-          };
+        rollback = {
+          magic = true;
         };
-      }
-    );
+        magicRollback = true;
+      };
+    };
 }
