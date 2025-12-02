7cb97042515b63d2900ee141d73f23acd0b5c4af
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-02 15:34:31 +0000

    Finished Step #1 - "preparer" ERROR ERROR: build step 1 "us-central1-doc

--- flake.nix
+++ flake.nix
@@ -1,48 +1,19 @@
 {
-  description = "A Next.js application for karaoke song requests.";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    deploy-rs.url = "github:serokell/deploy-rs";
-  };
-
-  outputs = { self, nixpkgs, deploy-rs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs_20
-          deploy-rs.packages.${system}.deploy-rs
-        ];
-      };
-
-      packages.${system}.deploy = deploy-rs.lib.${system}.mkDeployment {
-        inherit pkgs;
-        deployment.name = "singalong-karaoke-deployment";
-
-        steps = [
-          {
-            name = "Install Dependencies";
-            run = "npm install";
-          }
-          {
-            name = "Build Next.js App";
-            run = "npm run build";
-          }
-        ];
-
-        service = {
-          exec = "npm run start";
-          user = "root";
-        };
-
-        rollback = {
-          magic = true;
-        };
-        magicRollback = true;
-      };
-    };
+  "description": "A basic Node.js development environment for a Next.js application.",
+  "inputs": {
+    "nixpkgs": {
+      "url": "github:NixOS/nixpkgs/nixos-unstable"
+    }
+  },
+  "outputs": { "self, nixpkgs, ... }": {
+    "devShells": {
+      "x86_64-linux": {
+        "default": "nixpkgs.legacyPackages.x86_64-linux.mkShell {
+          "buildInputs": [
+            "nixpkgs.legacyPackages.x86_64-linux.nodejs_20"
+          ]
+        }"
+      }
+    }
+  }
 }
