e586bfda0a827f1eb95e3d63d069e033c46e4a4c
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-02 16:13:32 +0000

    🚀 ℹ️ [deploy] [INFO] Running checks for flake in . warning: Git tree '/

--- flake.nix
+++ flake.nix
@@ -1,19 +1,20 @@
 {
-  "description": "A basic Node.js development environment for a Next.js application.",
-  "inputs": {
-    "nixpkgs": {
-      "url": "github:NixOS/nixpkgs/nixos-unstable"
-    }
-  },
-  "outputs": { "self, nixpkgs, ... }": {
-    "devShells": {
-      "x86_64-linux": {
-        "default": "nixpkgs.legacyPackages.x86_64-linux.mkShell {
-          "buildInputs": [
-            "nixpkgs.legacyPackages.x86_64-linux.nodejs_20"
-          ]
-        }"
-      }
-    }
-  }
+  description = "A basic Node.js development environment for a Next.js application.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.nodejs_20
+        ];
+      };
+    };
 }
