602064ab9ff05537b759ccae642439976019723f
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-02 14:34:25 +0000

    error: attribute 'mkDeploy' missing        at /nix/store/wmjbjflq141n04j

--- flake.nix
+++ flake.nix
@@ -6,24 +6,32 @@
     deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, deploy-rs }:
+  outputs = { self, nixpkgs, deploy-rs, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
+        name = "karaoke-dev-shell";
+        packages = [
           pkgs.nodejs_20
-          self.packages.${system}.deploy
+          pkgs.deploy-rs
         ];
       };
 
-      packages.${system}.deploy = deploy-rs.lib.${system}.mkDeploy {
-        inherit pkgs;
-        main = ./apphosting.yaml;
+      deploy = {
+        nodes = {
+          app = {
+            hostname = "localhost";
+            deployment = deploy-rs.lib.${system}.mkDeployment {
+              inherit pkgs;
+              # Add specific deployment steps here if needed in the future
+            };
+          };
+        };
       };
-
-      deploy = self.packages.${system}.deploy;
     };
 }
