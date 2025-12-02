8e408486c03a75a3e94a2fdcdb81ad11f7549b6a
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-02 19:03:36 +0000

    error:        … while checking flake output 'packages'          at /nix/

--- flake.nix
+++ flake.nix
@@ -1,66 +1,21 @@
+
 {
-  description = "A flake for deploying a Next.js application with deploy-rs";
+  description = "A basic Node.js development environment for a Next.js application.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    deploy-rs.url = "github:serokell/deploy-rs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
-  outputs = { self, nixpkgs, flake-utils, deploy-rs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        
-        # Define the deployment using deploy-rs
-        deployment = deploy-rs.lib.mkDeployment {
-          nodes = {
-            # Since App Hosting is a single-node environment, we define one node.
-            app = {
-              # This assumes deployment via SSH is not needed as App Hosting handles it.
-              # We focus on the build steps.
-            };
-          };
-
-          steps = [
-            # Step 1: Copy the entire project source to the build environment.
-            deploy-rs.lib.steps.copy {
-              name = "copy-source";
-              node = "app";
-              src = ./.;
-              dest = "/app";
-            }
-            # Step 2: Run npm install and build inside the environment.
-            deploy-rs.lib.steps.run {
-              name = "build-app";
-              node = "app";
-              command = ''
-                cd /app
-                npm install
-                npm run build
-              '';
-            }
-          ];
-        };
-      in
-      {
-        packages = {
-          # This is the deploy attribute the build system is looking for.
-          deploy = deployment;
-
-          # Development shell for local use.
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.nodejs_20
-            ];
-          };
-        };
-
-        # This allows running 'nix run .#deploy' locally if needed for testing.
-        apps.deploy = deploy-rs.lib.apps.deploy {
-          inherit deployment;
-          node = "app";
-        };
-
-      });
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_20
+        ];
+      };
+    };
 }
--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,9 +1,7 @@
-
 "use client"
 
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
-import Image from "next/image"
 
 import { cn } from "@/lib/utils"
 
@@ -23,21 +21,16 @@ const Avatar = React.forwardRef<
 Avatar.displayName = AvatarPrimitive.Root.displayName
 
 const AvatarImage = React.forwardRef<
-  React.ElementRef<typeof Image>,
-  React.ComponentPropsWithoutRef<typeof Image>
+  React.ElementRef<typeof AvatarPrimitive.Image>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
 >(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Image asChild>
-    <Image
-      ref={ref}
-      fill
-      unoptimized
-      className={cn("aspect-square h-full w-full object-cover", className)}
-      {...props}
-    />
-  </AvatarPrimitive.Image>
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cn("aspect-square h-full w-full", className)}
+    {...props}
+  />
 ))
-AvatarImage.displayName = "AvatarImage"
-
+AvatarImage.displayName = AvatarPrimitive.Image.displayName
 
 const AvatarFallback = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Fallback>,
