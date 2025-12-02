86996c6dd23a13e05acd98648323dcb9c2759ce6
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-02 21:12:38 +0000

    error:        … while evaluating the attribute 'lib.x86_64-linux.mkDeplo

--- flake.nix
+++ flake.nix
@@ -1,49 +1,28 @@
 {
-  description = "A Nix flake for deploying a Next.js application with deploy-rs";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    deploy-rs.url = "github:serokell/deploy-rs";
-  };
-
-  outputs = { self, nixpkgs, deploy-rs }:
+  "description": "A basic Node.js development environment for a Next.js application.",
+  "inputs": {
+    "nixpkgs": {
+      "url": "github:NixOS/nixpkgs/nixos-unstable"
+    }
+  },
+  "outputs": { "self, nixpkgs":
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-
-      deployment = deploy-rs.lib.${system}.mkDeployment {
-        inherit pkgs;
-        deploymentName = "sing-a-long-karaoke";
-
-        steps = [
-          (deploy-rs.lib.steps.build {
-            src = ./.;
-            buildCommands = [
-              "npm install"
-              "npm run build"
-            ];
-          })
-          (deploy-rs.lib.steps.copy {
-            src = ./.;
-            path = "/app";
-          })
-        ];
-
-        services = {
-          app = {
-            command = "npm run start";
-            path = [ pkgs.nodejs-20_x ];
-          };
-        };
-      };
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
     in
     {
-      packages.${system}.deploy = deployment;
-
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs-20_x
-        ];
-      };
-    };
+      devShells = forEachSupportedSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs_20
+            ];
+          };
+        }
+      );
+    }
+  }
 }
