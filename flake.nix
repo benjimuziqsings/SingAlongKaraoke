5d0a8091bed7a0ca1a03ce6f0d7afa08a3cd473f
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-03 17:28:58 +0000

    ❌ [deploy] [ERROR] Failed to check deployment: Nix checking command resu

--- flake.nix
+++ flake.nix
@@ -1,62 +1,15 @@
 
 {
-  description = "Karaoke Queue Master Deployment";
-
+  description = "A basic Node.js development environment for a Next.js application.";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    deploy-rs.url = "github:serokell/deploy-rs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-
-  outputs = { self, nixpkgs, flake-utils, deploy-rs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-
-        deployment = deploy-rs.lib.mkDeployment {
-          inherit pkgs;
-          
-          deploymentName = "karaoke-queue-master-deployment";
-
-          steps = [
-            # Step 1: Copy source code to the deployment environment
-            (deploy-rs.lib.steps.copy {
-              name = "copy-source";
-              src = ./.;
-            })
-            # Step 2: Install npm dependencies
-            (deploy-rs.lib.steps.run {
-              name = "npm-install";
-              command = "npm install";
-            })
-            # Step 3: Build the Next.js application
-            (deploy-rs.lib.steps.run {
-              name = "npm-build";
-              command = "npm run build";
-            })
-          ];
-
-          services.app = {
-            # Define how to start the application
-            command = ["npm" "run" "start"];
-            # Ensure Node.js is available in the environment
-            path = [ pkgs.nodejs_20 ]; 
-          };
-        };
-      in
-      {
-        packages = {
-          # This is the deploy attribute the build system is looking for.
-          deploy = deployment;
-        };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs_20
-          ];
-        };
-      }
-    );
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = [
+        nixpkgs.legacyPackages.x86_64-linux.nodejs_20
+        nixpkgs.legacyPackages.x86_64-linux.nodePackages.npm
+      ];
+    };
+  };
 }
