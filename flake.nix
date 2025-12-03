31a14751b034a847475a8cc7cba6af2e973a0542
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-03 18:41:58 +0000

    warning: updating lock file '/home/user/studio/flake.lock': • Removed in

--- flake.nix
+++ flake.nix
@@ -1,34 +1,30 @@
-
 {
-  description = "A Next.js application for karaoke";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }:
+  "description": "Karaoke Queue Master",
+  "inputs": {
+    "nixpkgs": {
+      "url": "github:NixOS/nixpkgs/nixos-unstable"
+    },
+    "flake-utils": {
+      "url": "github:numtide/flake-utils"
+    }
+  },
+  "outputs": {
+    "self": _,
+    "nixpkgs": _,
+    "flake-utils": flake-utils,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        node = pkgs.nodejs-20_x;
       in
       {
-        # Defines a development shell with Node.js and npm.
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            node
-          ];
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodejs
+            ];
+          };
         };
-
-        # Defines the default application to run.
-        # This is what Firebase App Hosting uses to start your app.
-        apps.default = {
-          type = "app";
-          program = "${pkgs.writeShellScript "start-app" ''
-            exec ${node}/bin/npm run start
-          ''}";
-        };
-      }
-    );
+      })
 }
