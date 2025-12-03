f4f73cc4f68fd607fe1be956f243652f96aaa7aa
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-03 18:38:59 +0000

    error: … while checking flake output 'packages'  error: attribute 'mkDep

--- flake.nix
+++ flake.nix
@@ -1,60 +1,34 @@
+
 {
-  description = "A Next.js application for Sing A Long Karaoke";
+  description = "A Next.js application for karaoke";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, deploy-rs }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        deployment = deploy-rs.lib.${system}.mkDeployment {
-          inherit pkgs;
-          deploymentName = "sing-a-long-karaoke-deployment";
-
-          steps = [
-            (deploy-rs.lib.steps.build {
-              # build the Next.js app
-              buildCommand = ''
-                npm install
-                npm run build
-              '';
-              # specify the paths that should be copied to the deployment
-              copyPaths = [
-                { from = ./public; to = ./public; }
-                { from = ./package.json; to = ./package.json; }
-                { from = ./.next/standalone; to = ./.next/standalone; }
-                { from = ./.next/static; to = ./.next/static; }
-              ];
-            })
-
-            (deploy-rs.lib.steps.run {
-              # run the Next.js app
-              runCommand = "npm run start";
-
-              serviceConfig = {
-                # these are systemd service options
-                Restart = "always";
-                RestartSec = 5;
-              };
-            })
-          ];
-        };
+        pkgs = import nixpkgs { inherit system; };
+        node = pkgs.nodejs-20_x;
       in
       {
-        packages = {
-          # this is the deploy attribute the build system is looking for
-          deploy = deployment;
-        };
-
+        # Defines a development shell with Node.js and npm.
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            pkgs.nodejs_20
+            node
           ];
         };
-      });
+
+        # Defines the default application to run.
+        # This is what Firebase App Hosting uses to start your app.
+        apps.default = {
+          type = "app";
+          program = "${pkgs.writeShellScript "start-app" ''
+            exec ${node}/bin/npm run start
+          ''}";
+        };
+      }
+    );
 }
