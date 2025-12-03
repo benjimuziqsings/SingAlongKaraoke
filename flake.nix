819ec617156fa3f77ba95fbd552ae126b69f38da
Benji Muziq <benjimuziqsings@gmail.com>
2025-12-03 18:48:00 +0000

    error:        … while checking flake output 'packages'          at /nix/

--- flake.nix
+++ flake.nix
@@ -1,63 +1,65 @@
+
 {
-  description = "Sing A Long Karaoke";
+  description = "Sing A Long Karaoke Deployment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/f720de59066162ee879adcc8c79e15c51fe6bfb4";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, deploy-rs }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = { self, nixpkgs, deploy-rs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
 
-        deployment = deploy-rs.lib.${system}.mkDeployment {
-          inherit pkgs;
+      deployment = deploy-rs.lib.${system}.mkDeployment {
+        inherit pkgs;
+        deploymentName = "sing-a-long-karaoke-deployment";
 
-          deployment.name = "sing-a-long-karaoke-deployment";
-
-          # This is where we can specify what's in the deployment
-          deployment.build = {
-            # You can copy files from the flake to the deployment machine.
-            # The `self` parameter refers to the flake's root directory.
-            copy = [
-              self
-            ];
-          };
-
-          # The activation script is run once on the first deployment.
-          # Subsequent deployments will only run the activation script if the script itself has changed.
-          script = ''
-            mkdir -p /var/log/sing-a-long-karaoke
-            touch /var/log/sing-a-long-karaoke/app.log
-            
-            # Install npm dependencies
-            npm install
-            
-            # Build the Next.js app
-            npm run build
-          '';
-
-          # This specifies the systemd service to run.
-          # The service will be automatically started and enabled.
-          # It will also be restarted on subsequent deployments.
-          service = {
-            # This is the command that will be run to start the service.
-            # It will be run from the root of the deployment directory.
-            command = "${pkgs.nodejs}/bin/npm start";
-
-            # You can specify environment variables for the service.
-            # In this case we are just using the default PORT, but you could add more.
-            # environment = {
-            #   PORT = "3000";
-            # };
+        nodes = {
+          app = {
+            # This configuration does not define a real machine.
+            # It's used by the build system to understand how to
+            # build and run the application.
+            config = {
+              services.sing-a-long-karaoke = {
+                enable = true;
+                package = self.packages.${system}.default;
+              };
+            };
           };
         };
-      in
-      {
-        # This is the deploy attribute the build system is looking for.
-        packages.deploy = deployment;
-      }
-    );
+
+        magicRollback = false;
+      };
+    in
+    {
+      packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
+        name = "sing-a-long-karaoke";
+        src = ./.;
+        buildInputs = [ pkgs.nodejs-20_x ];
+        buildPhase = ''
+          export HOME=$(mktemp -d)
+          npm install
+          npm run build
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp -R . $out/
+        '';
+      };
+
+      # This is the deploy attribute the build system is looking for.
+      packages.x86_64-linux.deploy = deployment;
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs-20_x
+          pkgs.nodePackages.npm
+        ];
+      };
+    };
 }
